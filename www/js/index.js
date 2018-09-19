0d51bdc7c6235f4b69e171d59bd70df6056aadd1
Guilherme Bazilio <guilhermebazilio@gmail.com>
2018-09-19 20:14:35 -0300

    Binding onClick listeners to buttons
    Click each button will call it's corresponding method
    of `plugin.moments.<method>`.

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,15 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+function successCallback(message) {
+    alert(message);
+}
+
+function errorCallback(errorMessage) {
+    alert(errorMessage);
+}
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -34,9 +43,73 @@ var app = {
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
 
-        
+        var btnInitialize = document.getElementById('btnInitialize');
+        var btnRecordEvent = document.getElementById('btnRecordEvent');
+        var btnRecordEventWithData = document.getElementById('btnRecordEventWithData');
+        var btnFgTrackingMode = document.getElementById('btnFgTrackingMode');
+        var btnBgTrackingMode = document.getElementById('btnBgTrackingMode');
+        var btnLocation = document.getElementById('btnLocation');
+
+        btnInitialize.addEventListener('click', this.initMomentsSdk, false);
+        btnRecordEvent.addEventListener('click', this.recordEvent, false);
+        btnRecordEventWithData.addEventListener('click', this.recordEventWithData, false);
+        btnFgTrackingMode.addEventListener('click', this.setFgTrackingMode, false);
+        btnBgTrackingMode.addEventListener('click', this.setBgTrackingMode, false);
+        btnLocation.addEventListener('click', this.getBestKnownLocation, false);
 
         console.log('Received Event: ' + id);
+    }
+
+    initMomentsSdk: function() {
+        plugins.moments.initialize(successCallback, errorMessage);
+    },
+
+    recordEvent: function() {
+        plugins.moments.recordEvent("an_event", null, function (msg) {
+                alert(msg);
+            }, function (msg) {
+                alert(msg);
+            }
+        );
+    },
+
+    recordEventWithData: function() {
+        plugins.moments.recordEvent("an_event", 3.0, function (msg) {
+                alert(msg);
+            }, function (msg) {
+                alert(msg);
+            }
+        );
+    },
+
+    setFgTrackingMode: function() {
+        var trackingMode = plugins.moments.TrackingMode.MINIMAL_POWER;
+        plugins.moments.setFgTrackingMode(trackingMode, function (msg) {
+                alert(msg);
+            }, function (msg) {
+                alert(msg);
+            }
+        );
+    },
+
+    setBgTrackingMode: function() {
+        var trackingMode = plugins.moments.TrackingMode.STAY_DETECTION;
+        plugins.moments.setBgTrackingMode(trackingMode, function (msg) {
+                alert(msg);
+            }, function (msg) {
+                alert(msg);
+            }
+        );
+    },
+
+    getBestKnownLocation: function() {
+        var defaultSuccess = function (msg) {
+            alert(JSON.stringify(msg));
+        };
+        var defaultError = function (msg) {
+            alert(msg);
+        };
+        plugins.moments.bestKnownLocation(defaultSuccess, defaultError);
     }
 };
 
